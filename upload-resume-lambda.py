f176f6612394bca468cef5bd051664934a61a7a7
R B <rbrampton@yahoo.ca>
2018-11-18 23:49:51 -0500

    apply zip python

---
+++ upload-resume-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('resume.vrfuture.com')
+build_bucket = s3.Bucket('resumebuild.vrfuture.com')
+portfolio_zip = io.BytesIO()
+
+build_bucket.download_fileobj('BuildResume.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj=myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print(nm)
