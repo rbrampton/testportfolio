9cac91268ec4f1ae43ea4929176fe4fb5e64408d
R B <rbrampton@yahoo.ca>
2018-11-19 20:12:42 -0500

    build zip

--- upload-resume-lambda.py
+++ upload-resume-lambda.py
@@ -1,20 +1,25 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('resume.vrfuture.com')
-build_bucket = s3.Bucket('resumebuild.vrfuture.com')
-portfolio_zip = io.BytesIO()
+    s3 = boto3.resource('s3')
 
-build_bucket.download_fileobj('BuildResume.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('resume.vrfuture.com')
+    build_bucket = s3.Bucket('resumebuild.vrfuture.com')
+    portfolio_zip = io.BytesIO()
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj=myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        print(nm)
+    build_bucket.download_fileobj('BuildResume.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj=myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            print(nm)
+
+    return 'success'
